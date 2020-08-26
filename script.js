31724c5604ea3fcb082b8c16856a0f8f955e484d
cascutter <cascutter13@gmail.com>
2020-08-26 18:10:44 -0500

    Got uv index to diaply; 5 day displays

--- script.js
+++ script.js
@@ -32,50 +32,58 @@ $(document).ready(function () {
 
             previousCity();
             getCurrentConditions(data);
+            getForecast(data)
+            //getUvIndex(data);
             
         });
-
-        // let uviQueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid={&appid=6ca038ea2b4c13fe63caf34632dc9f40}&lat={lat}&lon={lon}";
-        // //let lat = data.city.coord.lat;
-        // //let lon = data.city.coord.lon;
-
-        // $.ajax({
-        //     url: uviQueryURL,
-        //     method: "GET"
-        // })
-        // .then(function (data){
-        //     let uvIndex = data.value;
-            
-        //     previousCity();
-        //     getCurrentConditions(data);
-            
-        //     console.log(uvIndex);
-        // })
+       
     });
 
     // Adds previously searched cities to search bar card
     function previousCity() {
-        const cityListItem = $("<li>").addClass("list-group-item").text(city);
+        let cityListItem = $("<li>").addClass("list-group-item").text(city);
         $(".list").prepend(cityListItem);
+    }
+
+    function getUvIndex(data) {
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
+
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/uvi?" + apiKey + "&lat=" + lat + "&lon=" + lon,
+            method: "GET"
+        })
+        .then (function (data){
+            console.log(data);
+            console.log(data.value);
+            let uvIndex = $("<p>").addClass("card-text current-uv").text("UV Index: ");
+            let uvValue = $("<span>").text(data.value);
+            // Update class and add if statements to change background color of button
+            $("#current-city .card-body").append(uvIndex.append(uvValue));        
+        })
+
     }
     
     // Gets current conditions from city search
     function getCurrentConditions(data) {
 
-        // Translates temp from kevlin to farhenheit
-        const tempF = (data.main.temp - 273.15) * 1.80 + 32;
+        getUvIndex(data);
+        //console.log(uvData);
+
+        // Calculates temp from kevlin to farhenheit
+        let tempF = (data.main.temp - 273.15) * 1.80 + 32;
         tempF = Math.floor(tempF);
 
         // Uses moment.js to get current date
         let currentDate = moment().subtract(10, 'days').calendar();
 
-        // API call for weather icons
+        // Get weather icons
         let iconCode = data.weather[0].icon;
         let iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
         $(".icon").html("<img src='" + iconURL + "'>");
 
         // Empty card 
-        $(".current-city").empty();
+        $("#current-city").empty();
 
         // Establishing variables for data from api call and card
         let currentCityCard = $("<div>").addClass("card");
@@ -86,23 +94,67 @@ $(document).ready(function () {
         let temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + "°F");
         let humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + data.main.humidity + "%");
         let windSpeed = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + data.wind.speed + " MPH");
-        //let uvIndex = $("<p>").addClass("card-text current-uv").text("UV Index: " + data.uvindex);
+        //let uvIndex = $("<p>").addClass("card-text current-uv").text("UV Index: " + data.value);
 
         // Append data to current city card
         city.append(date, iconDisplay);
         cardBody.append(city, temperature, humidity, windSpeed);
         currentCityCard.append(cardBody);
-        $(".current-city").append(currentCityCard);
+        $("#current-city").append(currentCityCard);
     }
 
+    $(function() {
+        loadData();
+            function loadData() {
+                $("#current-city").val(localStorage.getItem("card-text"));
+            }
+        $("#searchBtn").click(function() {
+            let currentData = $(this).siblings("card-text").val("");
+            let cardData = $(this).siblings("card-text").attr("id");
+            localStorage.setItem(cardData, currentData);
+        })
+    })
+
+    function getForecast () {
+  
+        $.ajax({
+          url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+          method: "GET"
+        }).then(function (data){
+          $('#forecast').empty();
+          console.log(data);
+      
+          let results = data.list;
     
-
-
-
-
-
-
-
+          for (let i = 0; i < results.length; i++) {
+            console.log(results[i].dt_txt)
+            let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+            let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+            console.log(day);
+            console.log(hour);
+      
+            if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+              
+              // get the temperature and convert to fahrenheit 
+              let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+              let tempF = Math.floor(temp);
+      
+              let forecastCard = $("<div>").addClass("card col-md-2 ml-3 bg-primary text-white");
+              let forecastCardBody = $("<div>").addClass("card-body p-3 forecastBody")
+              let forecastDate = $("<h4>").addClass("card-title").text(day);
+              let forecastTemp = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+              let forecastHumidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+      
+              //let image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+      
+              forecastCardBody.append(forecastDate, forecastTemp, forecastHumidity);
+              forecastCard.append(forecastCardBody);
+              $("#forecast").append(forecastCard);
+      
+            }
+          }
+        });
+    }
 
 
 })
